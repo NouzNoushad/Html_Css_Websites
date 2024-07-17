97c9deab4bc576a41cc13360f78ec6177abbab9f
unknown <nhd7noushadazeez@gmail.com>
2024-07-17 21:45:29 +0530

    about section

--- guarder_site/js/script.js
+++ guarder_site/js/script.js
@@ -1,6 +1,8 @@
 const navBar = document.querySelector(".main-nav");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const mainNav = document.querySelector("header .main-nav")
+const scrollBtn =document.querySelector(".scroll-btn a");
 
 
 menuBtn.onclick = function () {
@@ -9,4 +11,14 @@ menuBtn.onclick = function () {
 
 cancelBtn.onclick = function () {
     navBar.classList.remove("active");
+}
+
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        mainNav.classList.add("sticky");
+        scrollBtn.style.display = "block";
+    }else{
+        mainNav.classList.remove("sticky")
+        scrollBtn.style.display = "none";
+    }
 }
