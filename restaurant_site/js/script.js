c3003eac50c18f13345f5a4da7f04f9079e8ceb8
unknown <nhd7noushadazeez@gmail.com>
2024-07-20 11:51:38 +0530

    menu section

--- restaurant_site/js/script.js
+++ restaurant_site/js/script.js
@@ -1,6 +1,8 @@
 const navBar = document.querySelector(".nav-bar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const scrollBtn = document.querySelector(".scroll-btn a");
+const header = document.querySelector("header");
 
 menuBtn.onclick = function () {
     navBar.classList.add("active");
@@ -8,6 +10,16 @@ menuBtn.onclick = function () {
 
 cancelBtn.onclick = function () {
     navBar.classList.remove("active");
+}
+
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        scrollBtn.style.display = "block";
+        header.classList.add("sticky");
+    }else {
+        scrollBtn.style.display = "none";
+        header.classList.remove("sticky");
+    }
 }
 
 // Swiper
