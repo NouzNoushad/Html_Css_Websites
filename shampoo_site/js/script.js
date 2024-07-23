6e7d6a1cad52cf97690ea1400623c8277e477e47
unknown <nhd7noushadazeez@gmail.com>
2024-07-23 22:50:56 +0530

    scroll up button

--- shampoo_site/js/script.js
+++ shampoo_site/js/script.js
@@ -1,6 +1,8 @@
 const navContent = document.querySelector(".navbar .nav-content");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const scrollBtn = document.querySelector(".scroll-btn a");
+const nav = document.querySelector("nav");
 
 menuBtn.onclick = function() {
     navContent.classList.add("active");
@@ -8,4 +10,14 @@ menuBtn.onclick = function() {
 
 cancelBtn.onclick = function() {
     navContent.classList.remove("active");
+}
+
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        scrollBtn.style.display = "block";
+        nav.classList.add("sticky");
+    }else{
+        scrollBtn.style.display = "none";
+        nav.classList.remove("sticky");
+    }
 }
