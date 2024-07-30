bdc62a6e262830744a987f2745936a288a0681d3
unknown <nhd7noushadazeez@gmail.com>
2024-07-30 21:45:41 +0530

    scorll up  button

--- tea_house_site/js/script.js
+++ tea_house_site/js/script.js
@@ -2,7 +2,6 @@ const navLinks = document.querySelector("nav .nav-links");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const scrollBtn = document.querySelector(".scroll-btn a");
-const nav = document.querySelector("nav");
 
 menuBtn.onclick = function() {
     navLinks.classList.add("active");
@@ -10,4 +9,12 @@ menuBtn.onclick = function() {
 
 cancelBtn.onclick = function() {
     navLinks.classList.remove("active");
+}
+
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        scrollBtn.style.display = "block";
+    }else{
+        scrollBtn.style.display = "none";
+    }
 }
