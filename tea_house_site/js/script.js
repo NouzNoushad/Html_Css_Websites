5b3b2b732d1287b57e1b1319086e47f91bdcbb4d
unknown <nhd7noushadazeez@gmail.com>
2024-07-31 21:44:51 +0530

    about section

--- tea_house_site/js/script.js
+++ tea_house_site/js/script.js
@@ -2,6 +2,7 @@ const navLinks = document.querySelector("nav .nav-links");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const scrollBtn = document.querySelector(".scroll-btn a");
+const nav = document.querySelector("nav");
 
 menuBtn.onclick = function() {
     navLinks.classList.add("active");
@@ -14,7 +15,9 @@ cancelBtn.onclick = function() {
 window.onscroll = function() {
     if(document.documentElement.scrollTop > 20){
         scrollBtn.style.display = "block";
+        nav.classList.add("sticky");
     }else{
         scrollBtn.style.display = "none";
+        nav.classList.remove("sticky");
     }
 }
